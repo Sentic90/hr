4b68f4f2777dbb2d1623d141ede49975d0fa0af4
sentic90 <priceali5549@gmail.com>
2021-01-20 22:31:47 +0200

    Add import_export editing.

--- employee/admin.py
+++ employee/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from .models import Employee
+from import_export.admin import ImportExportActionModelAdmin
 
 @admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
+class EmployeeAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['Fname', 'Minit', 'Lname', 'ssn', 'Bdate', 'Address', 'Sex', 'Salary', 'Super_ssn', 'dno']
     search_fields = ["ssn", "Address"]
     list_filter =  ["Bdate", "Minit", "Sex", "Salary"]
     search_fields = ['ssn', 'Salary']
     # readonly_fields = 
     # ordering = 
-    # list_editable = ['']
+    list_editable = ['ssn']
     # fields
     # fieldsets = 
     # fieldsets_add
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third_party
+    'import_export',
     #myapps:
     'employee',
     'department',
